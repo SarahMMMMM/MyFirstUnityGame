ac1c815eae9598945999fc49fcd2e70b81a6f350
SarahMMMMM <94809661+SarahMMMMM@users.noreply.github.com>
2023-12-29 20:47:31 -0500

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private bool jumpKeyPressed;
+    private float horizontalInput;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,24 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyPressed = true;
+        }
+
+        //horizontalInput = Input.GetAxis("horizontal");
+
+    }
+
+    //Fixed Update is called once every physics update
+    private void FixedUpdate()
+    {
+        if (jumpKeyPressed)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);   //this is a method
+            jumpKeyPressed = false;        
+        }
+
+        //GetComponent<Rigidbody>().velocity = new Vector3(horizontalInput, 0, 0);
     }
 }
